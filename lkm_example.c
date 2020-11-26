8555b8919a2841b724b6c1c225b4d9966b47b134
Brian Herman <bherma3@icloud.com>
2020-11-25 19:52:02 -0600

    Initial Commit.

---
+++ lkm_example.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("MIT");
+MODULE_AUTHOR("Brian Herman");
+MODULE_DESCRIPTION("");
+MODULE_VERSION("0.01");
+
+static int __init lkm_example_init(void)
+{
+    printk(KERN_INFO, "Hello world!\n");
+    return 0;
+}
+
+static void __exit lkm_example_exit(void)
+{
+    printk(KERN_INFO, "Goodbye!");
+}
+
+module_init(lkm_example_init);
+module_exit(lkm_example_exit);
